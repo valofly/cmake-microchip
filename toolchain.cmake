30fdfd0ab4a9d1749cd1b37142b0425b8ba17ba7
Arne Glaser <arne.glaser@valofly.com>
2026-04-22 12:43:54 +0200

    add support for AVR DB family

--- toolchain.cmake
+++ toolchain.cmake
@@ -64,6 +64,7 @@ list(APPEND MICROCHIP_FAMILIES_8
     ATxmega
     ATmega
     AVRDA
+    AVRDB
 )
 
 # known 16-bit MCU families
@@ -126,7 +127,7 @@ elseif(MICROCHIP_MCU MATCHES "^(AT)(tiny|mega|xmega)([a-zA-Z0-9]+)$")
         )
     endif()
     
-elseif(MICROCHIP_MCU MATCHES "^(AVR)([0-9]+)(DA)([0-9]+)$")
+elseif(MICROCHIP_MCU MATCHES "^(AVR)([0-9]+)(DA|DB)([0-9]+)$")
     set(MICROCHIP_MCU_FAMILY "${CMAKE_MATCH_1}${CMAKE_MATCH_3}")
     set(MICROCHIP_MCU_MODEL  "${MICROCHIP_MCU}")
     if(MICROCHIP_MCU_FAMILY IN_LIST MICROCHIP_FAMILIES_8)
